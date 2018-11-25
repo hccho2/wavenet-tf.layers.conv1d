7724d2954c518345932905249057fd4d559b9bf5
Heecheol Cho <hccho2@gmail.com>
2018-11-25 16:32:53 +0900

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,228 @@
+#  coding: utf-8
+"""
+sample_rate = 16000이므로, samples 48000이면 3초 길이가 된다.
+--fast_generation False --> 엄청 느림
+
+> python generate.py ./logdir/train/2018-11-01T22-46-56/model.ckpt-102000
+> python generate.py --samples 48000 --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-11-25T14-10-48/model.ckpt-60
+
+
+--samples 16000 --gc_cardinality 2 --gc_id 1 --wav_seed ./logdir/seed2.wav ./logdir/train/2018-11-17T09-33-24/model.ckpt-12000
+--samples 16000 --gc_cardinality 2 --gc_id 1 --wav_seed ./logdir/seed2.wav ./logdir/train/2018-11-17T09-33-24/model.ckpt-12000
+
+
+wav_seed를 줄때는 samples 갯수보다 길어야 한다.
+> python generate.py --samples 48000 --wav_seed ./logdir/seed.wav ./logdir/train/2018-11-01T22-46-56/model.ckpt-102000
+
+> tensorboard --logdir=./logdir/generate/2018-11-02T22-31-27
+"""
+import argparse
+from datetime import datetime
+import json
+import os,time
+
+import librosa
+import numpy as np
+import tensorflow as tf
+
+from wavenet import WaveNetModel, mu_law_decode, mu_law_encode, audio_reader
+
+SAMPLES = 16000
+TEMPERATURE = 1.0
+LOGDIR = './logdir'
+WAVENET_PARAMS = './wavenet_params.json'
+SAVE_EVERY = 3  # SAVE_EVERY 초마다 저장
+SILENCE_THRESHOLD = 0  # 0.1
+GC_CHANNELS = 32 # gc_channels = embedding vector dim
+
+def get_arguments():
+    def _str_to_bool(s):
+        """Convert string to bool (in argparse context)."""
+        if s.lower() not in ['true', 'false']:
+            raise ValueError('Argument needs to be a boolean, got {}'.format(s))
+        return {'true': True, 'false': False}[s.lower()]
+
+    def _ensure_positive_float(f):
+        """Ensure argument is a positive float."""
+        if float(f) < 0:
+            raise argparse.ArgumentTypeError('Argument must be greater than zero')
+        return float(f)
+
+    parser = argparse.ArgumentParser(description='WaveNet generation script')
+    parser.add_argument('checkpoint', type=str, help='Which model checkpoint to generate from')
+    parser.add_argument('--samples',type=int,default=SAMPLES, help='How many waveform samples to generate')
+    parser.add_argument('--temperature', type=_ensure_positive_float, default=TEMPERATURE,help='Sampling temperature')
+    parser.add_argument('--logdir',type=str,default=LOGDIR,help='Directory in which to store the logging information for TensorBoard.')
+    parser.add_argument('--wavenet_params', type=str,default=WAVENET_PARAMS, help='JSON file with the network parameters')
+    parser.add_argument('--wav_out_path',type=str,default=None,help='Path to output wav file')
+    parser.add_argument('--save_every',type=int,default=SAVE_EVERY,help='How many samples before saving in-progress wav')
+
+    
+    
+    parser.add_argument('--wav_seed',type=str,default=None,help='The wav file to start generation from')
+    parser.add_argument('--gc_channels',type=int,default=GC_CHANNELS,help='Number of global condition embedding channels. Omit if no global conditioning.')
+    parser.add_argument('--gc_cardinality',type=int,default=None,help='Number of categories upon which we globally condition.')
+    parser.add_argument('--gc_id',type=int,default=None,help='ID of category to generate, if globally conditioned.')
+    
+    arguments = parser.parse_args()
+    if arguments.gc_channels is not None:
+        if arguments.gc_cardinality is None:
+            raise ValueError("Globally conditioning but gc_cardinality not specified. Use --gc_cardinality=377 for full VCTK corpus.")
+
+        if arguments.gc_id is None:
+            raise ValueError("Globally conditioning, but global condition was not specified. Use --gc_id to specify global condition.")
+
+    return arguments
+
+
+def write_wav(waveform, sample_rate, filename):
+    y = np.array(waveform)
+    librosa.output.write_wav(filename, y, sample_rate)
+    print('Updated wav file at {}'.format(filename))
+
+
+def create_seed(filename,sample_rate,quantization_channels,window_size,silence_threshold=SILENCE_THRESHOLD):
+    audio, _ = librosa.load(filename, sr=sample_rate, mono=True)
+    audio = audio_reader.trim_silence(audio, silence_threshold)
+
+    quantized = mu_law_encode(audio, quantization_channels)
+    
+    
+    # 짧으면 짧은 대로 return하는데, padding이라도 해야되지 않나???
+    cut_index = tf.cond(tf.size(quantized) < tf.constant(window_size), lambda: tf.size(quantized), lambda: tf.constant(window_size))
+
+    return quantized[:cut_index]
+
+
+def main():
+    args = get_arguments()
+    started_datestring = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
+    logdir = os.path.join(args.logdir, 'generate', started_datestring)
+    
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    with open(args.wavenet_params, 'r') as config_file:
+        wavenet_params = json.load(config_file)
+
+    with tf.device('/cpu:0'):
+
+        sess = tf.Session()
+        # batch size = 1만 작동함.
+        net = WaveNetModel(
+            batch_size=1,
+            dilations=wavenet_params['dilations'],
+            filter_width=wavenet_params['filter_width'],
+            residual_channels=wavenet_params['residual_channels'],
+            dilation_channels=wavenet_params['dilation_channels'],
+            quantization_channels=wavenet_params['quantization_channels'],
+            skip_channels=wavenet_params['skip_channels'],
+            use_biases=wavenet_params['use_biases'],
+            scalar_input=wavenet_params['scalar_input'],
+            initial_filter_width=wavenet_params['initial_filter_width'],
+            global_condition_channels=args.gc_channels,
+            global_condition_cardinality=args.gc_cardinality,train_mode=False)   # train 단계에서는 global_condition_cardinality를 AudioReader에서 파악했지만, 여기서는 넣어주어야 함
+    
+        samples = tf.placeholder(tf.int32,shape=[net.batch_size,1])  # samples: mu_law_encode로 변환된 것.  fast인 경우: 정수값 1개. list나 array아님.      fast가 아닌 경우: [128.0, 128.0, ..., 128.0, 178, 185]
+    
+
+        next_sample = net.predict_proba_incremental(samples, [args.gc_id]*net.batch_size)  # Fast Wavenet Generation Algorithm-1611.09482 algorithm 적용
+        
+            
+        var_list = [var for var in tf.global_variables() if 'queue' not in var.name ]
+        saver = tf.train.Saver(var_list)
+        print('Restoring model from {}'.format(args.checkpoint))
+        saver.restore(sess, args.checkpoint)
+        
+        sess.run(net.queue_initializer) # 이 부분이 없으면, checkpoint에서 복원된 값들이 들어 있다.
+    
+        decode = mu_law_decode(samples, wavenet_params['quantization_channels'])
+    
+        quantization_channels = wavenet_params['quantization_channels']
+        if args.wav_seed:
+            # wav_seed의 길이가 receptive_field보다 작으면, padding이라도 해야 되는 거 아닌가? 그냥 짧으면 짧은 대로 return함  --> 그래서 너무 짧으면 error
+            seed = create_seed(args.wav_seed,wavenet_params['sample_rate'],quantization_channels,net.receptive_field)  # --> mu_law encode 된 것.
+            waveform = sess.run(seed).tolist()  # [116, 114, 120, 121, 127, ...]
+            
+        else:
+            # Silence with a single random sample at the end.
+            waveform = [quantization_channels / 2] * (net.receptive_field - 1)  # 필요한 receptive_field 크기보다 1개 작게 만든 후, 아래에서 random하게 1개를 덧붙힌다.
+            waveform = np.array(waveform*net.batch_size).reshape(net.batch_size,-1)
+            waveform = np.concatenate([waveform,np.random.randint(quantization_channels,size=net.batch_size).reshape(net.batch_size,-1)],axis=-1)  # one hot 변환 전
+        
+    
+        if args.wav_seed:  # seed wav가 주어진 경우.
+            # When using the incremental generation, we need to
+            # feed in all priming samples one by one before starting the
+            # actual generation.
+            # TODO This could be done much more efficiently by passing the waveform
+            # to the incremental generator as an optional argument, which would be
+            # used to fill the queues initially.
+
+    
+            print('Priming generation...')
+            for i, x in enumerate(waveform[-net.receptive_field: -1]):  # 제일 마지막 1개는 아래의 for loop의 첫 loop에서 넣어준다.
+                if i % 100 == 0:
+                    print('Priming sample {}'.format(i))
+                sess.run(next_sample, feed_dict={samples: x})
+            print('Done.')
+    
+        last_sample_timestamp = datetime.now()
+        for step in range(args.samples):  # 원하는 길이를 구하기 위해 loop 
+
+            # 아래outputs를 만드는 2줄은 for 밖으로 빼도 되는 것 아닌가?
+            window = waveform[:,-1:]  # 제일 끝에 있는 1개만 samples에 넣어 준다.
+
+    
+            # Run the WaveNet to predict the next sample.
+            
+            # fast가 아닌경우. window: [128.0, 128.0, ..., 128.0, 178, 185]
+            # fast인 경우, window는 숫자 1개.
+            prediction = sess.run(next_sample, feed_dict={samples: window})  # samples는 mu law encoding된 것. 계산 과정에서 one hot으로 변환된다.  --> (batch_size,256)
+    
+            # Scale prediction distribution using temperature.
+            # 다음 과정은 args.temperature==1이면 각 원소를 합으로 나누어주는 것에 불과. 이미 softmax를 적용한 겂이므로, 합이 1이된다. 그래서 값의 변화가 없다.
+            # args.temperature가 1이 아니며, 각 원소의 log취한 값을 나눈 후, 합이 1이 되도록 rescaling하는 것이 된다.
+            np.seterr(divide='ignore')
+            scaled_prediction = np.log(prediction) / args.temperature   # args.temperature인 경우는 값의 변화가 없다.
+            scaled_prediction = (scaled_prediction - np.logaddexp.reduce(scaled_prediction,axis=-1,keepdims=True))  # np.log(np.sum(np.exp(scaled_prediction)))
+            scaled_prediction = np.exp(scaled_prediction)
+            np.seterr(divide='warn')
+    
+            # Prediction distribution at temperature=1.0 should be unchanged after
+            # scaling.
+            if args.temperature == 1.0:
+                np.testing.assert_allclose( prediction, scaled_prediction, atol=1e-5, err_msg='Prediction scaling at temperature=1.0 is not working as intended.')
+    
+            sample=[]
+            for i in range(net.batch_size):
+                sample.append(np.random.choice(np.arange(quantization_channels), p=scaled_prediction[i]))  # 숫자 하나 선택
+            waveform = np.concatenate([waveform,np.array(sample).reshape(net.batch_size,-1)],axis=-1)
+    
+            # Show progress only once per second.
+            current_sample_timestamp = datetime.now()
+            time_since_print = current_sample_timestamp - last_sample_timestamp
+            if time_since_print.total_seconds() > 1.:
+                print('Sample {:3<d}/{:3<d}'.format(step + 1, args.samples), end='\r')
+                last_sample_timestamp = current_sample_timestamp
+    
+            # If we have partial writing, save the result so far.
+            if (args.wav_out_path and args.save_every and (step + 1) % args.save_every == 0):
+                out = sess.run(decode, feed_dict={samples: waveform})
+                write_wav(out, wavenet_params['sample_rate'], args.wav_out_path)
+    
+        # Introduce a newline to clear the carriage return from the progress.
+        print()
+    
+   
+        # Save the result as a wav file.
+        for i in range(net.batch_size):
+            args.wav_out_path= logdir + '/test-{}.wav'.format(i)
+            write_wav(waveform[i], wavenet_params['sample_rate'], args.wav_out_path)
+    
+        print('Finished generating. The result can be viewed in TensorBoard.')
+
+
+if __name__ == '__main__':
+    s = time.time()
+    main()
+    print(time.time()-s,'sec')
---
+++ train.py
@@ -0,0 +1,313 @@
+#  coding: utf-8
+"""Training script for the WaveNet network on the VCTK corpus.
+
+This script trains a network with the WaveNet using data from the VCTK corpus,
+which can be freely downloaded at the following site (~10 GB):
+http://homepages.inf.ed.ac.uk/jyamagis/page3/page58/page58.html
+"""
+
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import json
+import os
+import sys
+import time
+from glob import glob
+import tensorflow as tf
+from tensorflow.python.client import timeline
+
+from wavenet import WaveNetModel, AudioReader, optimizer_factory
+tf.logging.set_verbosity(tf.logging.ERROR)
+BATCH_SIZE = 1
+
+DATA_DIRECTORY =  'D:\\hccho\\multi-speaker-tacotron-tensorflow-master\\datasets\son\\audio'   #   './VCTK-Corpus'
+
+
+LOGDIR_ROOT = './logdir'
+
+
+#LOGDIR = None
+LOGDIR = './/logdir//train//2018-11-25T14-10-48'   # son
+#LOGDIR = './/logdir//train//2018-11-08T21-09-51'   # test
+
+GC_CHANNELS = 32 # gc_channels = embedding vector dim
+
+CHECKPOINT_EVERY = 2000   # checkpoint 저장 주기
+NUM_STEPS = 200000  # 최대 step
+LEARNING_RATE = 1e-3
+WAVENET_PARAMS = './wavenet_params.json'
+STARTED_DATESTRING = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
+SAMPLE_SIZE = 100000
+L2_REGULARIZATION_STRENGTH = 0
+SILENCE_THRESHOLD = 0
+EPSILON = 0.001
+MOMENTUM = 0.9
+MAX_TO_KEEP = 3 # 최대 5개의 checkpoint만 저장되도록 
+METADATA = False
+
+
+def get_arguments():
+    def _str_to_bool(s):
+        """Convert string to bool (in argparse context)."""
+        if s.lower() not in ['true', 'false']:
+            raise ValueError('Argument needs to be a boolean, got {}'.format(s))
+        return {'true': True, 'false': False}[s.lower()]
+
+    parser = argparse.ArgumentParser(description='WaveNet example network')
+    parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
+                        help='How many wav files to process at once. Default: ' + str(BATCH_SIZE) + '.')
+    parser.add_argument('--data_dir', type=str, default=DATA_DIRECTORY, help='The directory containing the VCTK corpus.')
+    parser.add_argument('--store_metadata', type=bool, default=METADATA,help='Whether to store advanced debugging information (execution time, memory consumption) for use with '
+                        'TensorBoard. Default: ' + str(METADATA) + '.')
+    parser.add_argument('--logdir', type=str, default=LOGDIR,
+                        help='Directory in which to store the logging information for TensorBoard. '
+                        'If the model already exists, it will restore the state and will continue training. '
+                        'Cannot use with --logdir_root and --restore_from.')
+    parser.add_argument('--logdir_root', type=str, default=None,
+                        help='Root directory to place the logging output and generated model. These are stored '
+                        'under the dated subdirectory of --logdir_root. Cannot use with --logdir.')
+    parser.add_argument('--restore_from', type=str, default=None,
+                        help='Directory in which to restore the model from. This creates the new model under the dated directory '
+                        'in --logdir_root. Cannot use with --logdir.')
+    parser.add_argument('--checkpoint_every', type=int, default=CHECKPOINT_EVERY,help='How many steps to save each checkpoint after. Default: ' + str(CHECKPOINT_EVERY) + '.')
+    parser.add_argument('--num_steps', type=int, default=NUM_STEPS,help='Number of training steps. Default: ' + str(NUM_STEPS) + '.')
+    parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE,help='Learning rate for training. Default: ' + str(LEARNING_RATE) + '.')
+    parser.add_argument('--wavenet_params', type=str, default=WAVENET_PARAMS,help='JSON file with the network parameters. Default: ' + WAVENET_PARAMS + '.')
+    parser.add_argument('--sample_size', type=int, default=SAMPLE_SIZE, help='Concatenate and cut audio samples to this many samples. Default: ' + str(SAMPLE_SIZE) + '.')
+    parser.add_argument('--l2_regularization_strength', type=float,default=L2_REGULARIZATION_STRENGTH,
+                        help='Coefficient in the L2 regularization. Default: False')
+    parser.add_argument('--silence_threshold', type=float, default=SILENCE_THRESHOLD,
+                        help='Volume threshold below which to trim the start and the end from the training set samples. Default: ' + str(SILENCE_THRESHOLD) + '.')
+    parser.add_argument('--optimizer', type=str, default='adam', choices=optimizer_factory.keys(), help='Select the optimizer specified by this option. Default: adam.')
+    parser.add_argument('--momentum', type=float,default=MOMENTUM, help='Specify the momentum to be used by sgd or rmsprop optimizer. Ignored by the '
+                        'adam optimizer. Default: ' + str(MOMENTUM) + '.')
+    parser.add_argument('--histograms', type=_str_to_bool, default=False,help='Whether to store histogram summaries. Default: False')
+    
+    # global_condition_vector의 차원. 이것 지정함으로써, global conditioning을 모델에 반영하라는 의미가 된다.
+    parser.add_argument('--gc_channels', type=int, default=GC_CHANNELS, help='Number of global condition channels. Default: None. Expecting: Int')
+    parser.add_argument('--max_checkpoints', type=int, default=MAX_TO_KEEP, help='Maximum amount of checkpoints that will be kept alive. Default: '   + str(MAX_TO_KEEP) + '.')
+    return parser.parse_args()
+
+
+def save(saver, sess, logdir, step):
+    model_name = 'model.ckpt'
+    checkpoint_path = os.path.join(logdir, model_name)
+    print('Storing checkpoint to {} ...'.format(logdir), end="")
+    sys.stdout.flush()
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    saver.save(sess, checkpoint_path, global_step=step)
+    print(' Done.')
+
+
+def load(saver, sess, logdir):
+    print("Trying to restore saved checkpoints from {} ...".format(logdir),end="")
+
+    ckpt = tf.train.get_checkpoint_state(logdir)
+    #ckpt = get_most_recent_checkpoint(logdir)
+    if ckpt:
+        print("  Checkpoint found: {}".format(ckpt.model_checkpoint_path))
+        global_step = int(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
+        print("  Global step was: {}".format(global_step))
+        print("  Restoring...", end="")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+        print(" Done.")
+        return global_step
+    else:
+        print(" No checkpoint found.")
+        return None
+
+
+def get_default_logdir(logdir_root):
+    logdir = os.path.join(logdir_root, 'train', STARTED_DATESTRING)
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)    
+    return logdir
+
+
+def validate_directories(args):
+    """Validate and arrange directory related arguments."""
+
+    # Validation
+    if args.logdir and args.logdir_root:
+        raise ValueError("--logdir and --logdir_root cannot be "
+                         "specified at the same time.")
+
+    if args.logdir and args.restore_from:
+        raise ValueError(
+            "--logdir and --restore_from cannot be specified at the same "
+            "time. This is to keep your previous model from unexpected "
+            "overwrites.\n"
+            "Use --logdir_root to specify the root of the directory which "
+            "will be automatically created with current date and time, or use "
+            "only --logdir to just continue the training from the last "
+            "checkpoint.")
+
+    # Arrangement
+    logdir_root = args.logdir_root
+    if logdir_root is None:
+        logdir_root = LOGDIR_ROOT
+
+    logdir = args.logdir
+    if logdir is None:
+        logdir = get_default_logdir(logdir_root)
+        print('Using default logdir: {}'.format(logdir))
+
+    restore_from = args.restore_from
+    if restore_from is None:
+        # args.logdir and args.restore_from are exclusive,
+        # so it is guaranteed the logdir here is newly created.
+        restore_from = logdir
+
+    return {
+        'logdir': logdir,
+        'logdir_root': args.logdir_root,
+        'restore_from': restore_from
+    }
+
+def get_most_recent_checkpoint(checkpoint_dir):
+    checkpoint_paths = [path for path in glob("{}/*.ckpt-*.data-*".format(checkpoint_dir))]
+    idxes = [int(os.path.basename(path).split('-')[1].split('.')[0]) for path in checkpoint_paths]
+
+    max_idx = max(idxes)
+    lastest_checkpoint = os.path.join(checkpoint_dir, "model.ckpt-{}".format(max_idx))
+
+    #latest_checkpoint=checkpoint_paths[0]
+    print(" [*] Found lastest checkpoint: {}".format(lastest_checkpoint))
+    return lastest_checkpoint
+def main():
+    args = get_arguments()
+
+    try:
+        directories = validate_directories(args)
+    except ValueError as e:
+        print("Some arguments are wrong:")
+        print(str(e))
+        return
+
+    logdir = directories['logdir']
+    restore_from = directories['restore_from']
+
+    # Even if we restored the model, we will treat it as new training
+    # if the trained model is written into an arbitrary location.
+    is_overwritten_training = logdir != restore_from
+
+    with open(args.wavenet_params, 'r') as f:
+        wavenet_params = json.load(f)
+
+    # Create coordinator.
+    coord = tf.train.Coordinator()
+
+    # Load raw waveform from VCTK corpus.
+    with tf.name_scope('create_inputs'):
+        # Allow silence trimming to be skipped by specifying a threshold near
+        # zero.
+        silence_threshold = args.silence_threshold if args.silence_threshold > EPSILON else None
+        gc_enabled = args.gc_channels is not None
+        
+        # AudioReader에서 wav 파일을 잘라 input값을 만든다. receptive_field길이만큼을 앞부분에 pad하거나 앞조각에서 가져온다. (receptive_field+ sample_size)크기로 자른다.
+        reader = AudioReader(args.data_dir,coord,sample_rate=wavenet_params['sample_rate'],gc_enabled=gc_enabled,
+                                receptive_field=WaveNetModel.calculate_receptive_field(wavenet_params["filter_width"], wavenet_params["dilations"],wavenet_params["scalar_input"], wavenet_params["initial_filter_width"]),
+                                sample_size=args.sample_size,silence_threshold=silence_threshold)
+        audio_batch = reader.dequeue(args.batch_size)  # (batch_size, ?, 1)
+        if gc_enabled:
+            gc_id_batch = reader.dequeue_gc(args.batch_size) # [1,2,2,1,...] <--- batch size 길이
+        else:
+            gc_id_batch = None
+
+    # Create network.
+    net = WaveNetModel(
+        batch_size=args.batch_size,
+        dilations=wavenet_params['dilations'],
+        filter_width=wavenet_params['filter_width'],
+        residual_channels=wavenet_params['residual_channels'],
+        dilation_channels=wavenet_params['dilation_channels'],
+        quantization_channels=wavenet_params['quantization_channels'],
+        skip_channels=wavenet_params['skip_channels'],
+        use_biases=wavenet_params['use_biases'],  #  True
+        scalar_input=wavenet_params['scalar_input'],
+        initial_filter_width=wavenet_params['initial_filter_width'],
+        histograms=args.histograms,
+        global_condition_channels=args.gc_channels,
+        global_condition_cardinality=reader.gc_category_cardinality,
+        train_mode=True)
+
+    if args.l2_regularization_strength == 0:
+        args.l2_regularization_strength = None
+        
+       
+    loss = net.loss(input_batch=audio_batch, global_condition_batch=gc_id_batch, l2_regularization_strength=args.l2_regularization_strength)
+     
+    optimizer = optimizer_factory[args.optimizer](learning_rate=args.learning_rate,momentum=args.momentum)
+    
+    trainable = tf.trainable_variables()
+    
+    optim = optimizer.minimize(loss, var_list=trainable)
+
+    run_metadata = tf.RunMetadata()
+
+    # Set up session
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=False))  # log_device_placement=False --> cpu/gpu 자동 배치.
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
+    # Saver for storing checkpoints of the model.
+    saver = tf.train.Saver(var_list=tf.global_variables(), max_to_keep=args.max_checkpoints)  # 최대 checkpoint 저장 갯수 지정
+
+    try:
+        saved_global_step = load(saver, sess, restore_from)  # checkpoint load
+        if is_overwritten_training or saved_global_step is None:
+            # The first training step will be saved_global_step + 1,
+            # therefore we put -1 here for new or overwritten trainings.
+            saved_global_step = -1
+
+    except:
+        print("Something went wrong while restoring checkpoint. We will terminate training to avoid accidentally overwriting the previous model.")
+        raise
+
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    reader.start_threads(sess)
+
+    step = None
+    last_saved_step = saved_global_step
+    try:
+        for step in range(saved_global_step + 1, args.num_steps+1):
+            start_time = time.time()
+            if args.store_metadata and step % 50 == 0:
+                # Slow run that stores extra information for debugging.
+                print('Storing metadata')
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                loss_value, _ = sess.run([loss, optim],options=run_options,run_metadata=run_metadata)
+
+                tl = timeline.Timeline(run_metadata.step_stats)
+                timeline_path = os.path.join(logdir, 'timeline.trace')
+                with open(timeline_path, 'w') as f:
+                    f.write(tl.generate_chrome_trace_format(show_memory=True))
+            else:
+                loss_value, _ = sess.run([loss, optim])
+
+            duration = time.time() - start_time
+            print('step {:d} - loss = {:.3f}, ({:.3f} sec/step)'.format(step, loss_value, duration))
+
+            if step % args.checkpoint_every == 0:
+                save(saver, sess, logdir, step)
+                last_saved_step = step
+
+    except KeyboardInterrupt:
+        # Introduce a line break after ^C is displayed so save message
+        # is on its own line.
+        print()
+    finally:
+        if step > last_saved_step:
+            save(saver, sess, logdir, step)
+        coord.request_stop()
+        coord.join(threads)
+
+
+if __name__ == '__main__':
+    main()
+    print('Done')
