c812292276a02af0be9399b4caec356d1c7260f1
Heecheol Cho <hccho2@gmail.com>
2018-11-26 00:22:51 +0900

    Update __init__.py

--- wavenet/__init__.py
+++ wavenet/__init__.py
@@ -1,5 +1,4 @@
 #  coding: utf-8
 from .model import WaveNetModel
 from .audio_reader import AudioReader
-from .ops import (mu_law_encode, mu_law_decode, time_to_batch,
-                  batch_to_time, causal_conv, optimizer_factory)
+from .ops import (mu_law_encode, mu_law_decode,optimizer_factory)
