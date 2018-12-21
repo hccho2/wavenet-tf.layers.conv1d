5d8166d39aba27928429276c83caf59ce0f4ae3a
Heecheol Cho <hccho2@gmail.com>
2018-12-21 12:16:14 +0900

    Add files via upload

--- generate.py
+++ generate.py
@@ -2,9 +2,8 @@
 """
 sample_rate = 16000이므로, samples 48000이면 3초 길이가 된다.
 
-> python generate.py --samples 48000 --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-11-25T14-10-48/model.ckpt-26000
-> python generate.py --samples 48000 --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-11-25T16-50-59/model.ckpt-40   <----- filter_width = 3인 경우
-> python generate.py --samples 16000 --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-11-30T22-22-58/model.ckpt-40   <----- scalar_input = true인 경우
+> python generate.py --samples 48000 --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-12-21T10-22-59
+> python generate.py --samples 16000 --wav_seed ./logdir/seed.wav --gc_cardinality 2 --gc_id 1 ./logdir/train/2018-12-21T10-22-59
 
 wav_seed를 줄때는 samples 갯수보다 길어야 한다.
 > python generate.py --samples 48000 --wav_seed ./logdir/seed.wav ./logdir/train/2018-11-01T22-46-56/model.ckpt-102000
@@ -22,11 +21,12 @@ import tensorflow as tf
 
 from wavenet import WaveNetModel, mu_law_decode, mu_law_encode, audio_reader
 from tensorboard.summary import audio
+from hparams import hparams
+from utils import load_hparams,load
 
 SAMPLES = 16000
 TEMPERATURE = 1.0
 LOGDIR = './logdir'
-WAVENET_PARAMS = './wavenet_params.json'
 SILENCE_THRESHOLD = 0  # 0.1
 GC_CHANNELS = 32 # gc_channels = embedding vector dim
 BATCH_SIZE = 2
@@ -44,11 +44,10 @@ def get_arguments():
         return float(f)
 
     parser = argparse.ArgumentParser(description='WaveNet generation script')
-    parser.add_argument('checkpoint', type=str, help='Which model checkpoint to generate from')
+    parser.add_argument('checkpoint_dir', type=str, help='Which model checkpoint to generate from')
     parser.add_argument('--samples',type=int,default=SAMPLES, help='How many waveform samples to generate')
     parser.add_argument('--temperature', type=_ensure_positive_float, default=TEMPERATURE,help='Sampling temperature')
     parser.add_argument('--logdir',type=str,default=LOGDIR,help='Directory in which to store the logging information for TensorBoard.')
-    parser.add_argument('--wavenet_params', type=str,default=WAVENET_PARAMS, help='JSON file with the network parameters')
     parser.add_argument('--wav_out_path',type=str,default=None,help='Path to output wav file')
 
     
@@ -93,32 +92,39 @@ def create_seed(filename,sample_rate,quantization_channels,window_size,scalar_in
 
 
 def main():
-    args = get_arguments()
+    config = get_arguments()
     started_datestring = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
-    logdir = os.path.join(args.logdir, 'generate', started_datestring)
+    logdir = os.path.join(config.logdir, 'generate', started_datestring)
     
     if not os.path.exists(logdir):
         os.makedirs(logdir)
-    with open(args.wavenet_params, 'r') as config_file:
-        wavenet_params = json.load(config_file)
+
+
+    load_hparams(hparams, config.checkpoint_dir)
+
+
+
+
+
 
     with tf.device('/cpu:0'):
 
         sess = tf.Session()
-        scalar_input = wavenet_params['scalar_input']
+        scalar_input = hparams.scalar_input
         net = WaveNetModel(
             batch_size=BATCH_SIZE,
-            dilations=wavenet_params['dilations'],
-            filter_width=wavenet_params['filter_width'],
-            residual_channels=wavenet_params['residual_channels'],
-            dilation_channels=wavenet_params['dilation_channels'],
-            quantization_channels=wavenet_params['quantization_channels'],
-            skip_channels=wavenet_params['skip_channels'],
-            use_biases=wavenet_params['use_biases'],
-            scalar_input=wavenet_params['scalar_input'],
-            initial_filter_width=wavenet_params['initial_filter_width'],
-            global_condition_channels=args.gc_channels,
-            global_condition_cardinality=args.gc_cardinality,train_mode=False)   # train 단계에서는 global_condition_cardinality를 AudioReader에서 파악했지만, 여기서는 넣어주어야 함
+            dilations=hparams.dilations,
+            filter_width=hparams.filter_width,
+            residual_channels=hparams.residual_channels,
+            dilation_channels=hparams.dilation_channels,
+            quantization_channels=hparams.quantization_channels,
+            out_channels =hparams.out_channels,
+            skip_channels=hparams.skip_channels,
+            use_biases=hparams.use_biases,
+            scalar_input=scalar_input,
+            initial_filter_width=hparams.initial_filter_width,
+            global_condition_channels=hparams.gc_channels,
+            global_condition_cardinality=config.gc_cardinality,train_mode=False)   # train 단계에서는 global_condition_cardinality를 AudioReader에서 파악했지만, 여기서는 넣어주어야 함
     
         if scalar_input:
             samples = tf.placeholder(tf.float32,shape=[net.batch_size,None])
@@ -126,22 +132,23 @@ def main():
             samples = tf.placeholder(tf.int32,shape=[net.batch_size,None])  # samples: mu_law_encode로 변환된 것. one-hot으로 변환되기 전. (batch_size, 길이)
     
 
-        next_sample = net.predict_proba_incremental(samples, [args.gc_id]*net.batch_size)  # Fast Wavenet Generation Algorithm-1611.09482 algorithm 적용
+        next_sample = net.predict_proba_incremental(samples, [config.gc_id]*net.batch_size)  # Fast Wavenet Generation Algorithm-1611.09482 algorithm 적용
         
             
         var_list = [var for var in tf.global_variables() if 'queue' not in var.name ]
         saver = tf.train.Saver(var_list)
-        print('Restoring model from {}'.format(args.checkpoint))
-        saver.restore(sess, args.checkpoint)
+        print('Restoring model from {}'.format(config.checkpoint_dir))
+        
+        load(saver, sess, config.checkpoint_dir)
         
         sess.run(net.queue_initializer) # 이 부분이 없으면, checkpoint에서 복원된 값들이 들어 있다.
     
         
     
-        quantization_channels = wavenet_params['quantization_channels']
-        if args.wav_seed:
+        quantization_channels = hparams.quantization_channels
+        if config.wav_seed:
             # wav_seed의 길이가 receptive_field보다 작으면, padding이라도 해야 되는 거 아닌가? 그냥 짧으면 짧은 대로 return함  --> 그래서 너무 짧으면 error
-            seed = create_seed(args.wav_seed,wavenet_params['sample_rate'],quantization_channels,net.receptive_field,scalar_input)  # --> mu_law encode 된 것.
+            seed = create_seed(config.wav_seed,hparams.sample_rate,quantization_channels,net.receptive_field,scalar_input)  # --> mu_law encode 된 것.
             if scalar_input:
                 waveform = seed.tolist()
             else:
@@ -167,7 +174,7 @@ def main():
             
     
         last_sample_timestamp = datetime.now()
-        for step in range(args.samples):  # 원하는 길이를 구하기 위해 loop 
+        for step in range(config.samples):  # 원하는 길이를 구하기 위해 loop 
 
             window = waveform[:,-1:]  # 제일 끝에 있는 1개만 samples에 넣어 준다.
 
@@ -182,17 +189,17 @@ def main():
                 sample = prediction
             else:
                 # Scale prediction distribution using temperature.
-                # 다음 과정은 args.temperature==1이면 각 원소를 합으로 나누어주는 것에 불과. 이미 softmax를 적용한 겂이므로, 합이 1이된다. 그래서 값의 변화가 없다.
-                # args.temperature가 1이 아니며, 각 원소의 log취한 값을 나눈 후, 합이 1이 되도록 rescaling하는 것이 된다.
+                # 다음 과정은 config.temperature==1이면 각 원소를 합으로 나누어주는 것에 불과. 이미 softmax를 적용한 겂이므로, 합이 1이된다. 그래서 값의 변화가 없다.
+                # config.temperature가 1이 아니며, 각 원소의 log취한 값을 나눈 후, 합이 1이 되도록 rescaling하는 것이 된다.
                 np.seterr(divide='ignore')
-                scaled_prediction = np.log(prediction) / args.temperature   # args.temperature인 경우는 값의 변화가 없다.
+                scaled_prediction = np.log(prediction) / config.temperature   # config.temperature인 경우는 값의 변화가 없다.
                 scaled_prediction = (scaled_prediction - np.logaddexp.reduce(scaled_prediction,axis=-1,keepdims=True))  # np.log(np.sum(np.exp(scaled_prediction)))
                 scaled_prediction = np.exp(scaled_prediction)
                 np.seterr(divide='warn')
         
                 # Prediction distribution at temperature=1.0 should be unchanged after
                 # scaling.
-                if args.temperature == 1.0:
+                if config.temperature == 1.0:
                     np.testing.assert_allclose( prediction, scaled_prediction, atol=1e-5, err_msg='Prediction scaling at temperature=1.0 is not working as intended.')
     
                 sample = [[np.random.choice(np.arange(quantization_channels), p=p)] for p in scaled_prediction]  # choose one sample per batch
@@ -203,7 +210,7 @@ def main():
             current_sample_timestamp = datetime.now()
             time_since_print = current_sample_timestamp - last_sample_timestamp
             if time_since_print.total_seconds() > 1.:
-                print('Sample {:3<d}/{:3<d}'.format(step + 1, args.samples), end='\r')
+                print('Sample {:3<d}/{:3<d}'.format(step + 1, config.samples), end='\r')
                 last_sample_timestamp = current_sample_timestamp
     
     
@@ -218,8 +225,8 @@ def main():
             decode = mu_law_decode(samples, quantization_channels)
             out = sess.run(decode, feed_dict={samples: waveform})
         for i in range(net.batch_size):
-            args.wav_out_path= logdir + '/test-{}.wav'.format(i)
-            write_wav(out[i], wavenet_params['sample_rate'], args.wav_out_path)
+            config.wav_out_path= logdir + '/test-{}.wav'.format(i)
+            write_wav(out[i], hparams.sample_rate, config.wav_out_path)
         
         print('Finished generating.')
 
